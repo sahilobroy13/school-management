94e6a89c6f3ede412d090437863d4d3d8f8c9bfe
sahil kumar <sahilobroy579@gmail.com>
2025-08-07 01:01:57 +0530

    Initialize the project with server and database setup

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const connection = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+})
+
+connection.connect(err =>{
+    if(err){
+        console.log("DB Connection Failed !" , err.message);
+    }else{
+        console.log("Sql database Connected successfully !");
+    }
+})
+
+module.exports = connection;
