bd3492bab346af531bfff6f829b3d95e2aa036f0
sahil kumar <sahilobroy579@gmail.com>
2025-08-07 23:10:20 +0530

    Did some changes

--- config/db.js
+++ config/db.js
@@ -8,6 +8,9 @@ const connection = mysql.createConnection({
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0
 })
 
 connection.connect(err =>{
