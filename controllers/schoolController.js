394306ba26d7198a93c4cbfa7005217297af2bd0
sahil kumar <sahilobroy579@gmail.com>
2025-08-08 01:01:33 +0530

    Fixed the bug

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -7,12 +7,12 @@ exports.addSchool = (req,res)=>{
         return res.status(400).json({message : "All fields are required !"});
     }
 
-    const sql = "INSERT INTO schools (name, adress, latitude, longitude) VALUES (?, ?, ?, ?)";
+    const sql = "INSERT INTO schools (name, adress, latitude, longitude) VALUES ($1, $2, $3, $4)";
     db.query(sql,[name,adress,latitude,longitude],(err,result)=>{
         if(err){
             return res.status(400).json({message: "Database Eroor !", err});
         }
-        res.status(201).json({ message: "School added successfully", schoolId: result.insertId });
+        res.status(201).json({ message: "School added successfully"});
 
     })
 }
