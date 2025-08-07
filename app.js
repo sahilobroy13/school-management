3284ccf3d783186b15100fbd682cbd3ef0347fc9
sahil kumar <sahilobroy579@gmail.com>
2025-08-07 11:03:07 +0530

    Successfully connected it with database

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const app =  express();
 const dotenv = require("dotenv");
 dotenv.config();
 
+const db = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
